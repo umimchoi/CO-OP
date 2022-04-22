b4d2f4744fa789344edde0df85093d079a67288d
Umim Choi <umimchoi@gmail.com>
2022-04-22 20:47:38 +0700

    init project

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+/*
+const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
+const appointments = require("./routes/appointments");
+*/
+app.use(express.json());
+app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+
+const limiter = rateLimit({
+  windowsMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+app.use(limiter);
+app.use(hpp());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(
+  PORT,
+  console.log("Server running in", process.env.NODE_ENV, " mode on port ", PORT)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
